22ab49d110026f6f669c15ce05d1387cda1ba8db
sylvan <sylvanlyon@gmail.com>
2025-08-08 19:42:03 +0800

    完成最基本的用户注册、登录和注销功能 并且放弃 Cargo.lock 的追踪

--- backend/sql/ddl/usr.sql
+++ backend/sql/ddl/usr.sql
@@ -4,9 +4,9 @@ CREATE SCHEMA "usr" AUTHORIZATION postgres;
 
 CREATE TABLE "usr"."usr_info" (
     "id" BIGSERIAL PRIMARY KEY,
-    "name" CHAR(32) NOT NULL,
+    "name" VARCHAR(32) NOT NULL,
     "email" VARCHAR(256) UNIQUE CHECK ("email" ~* '^[\w._%+-]+@[\w.-]+\.\w{2,}$'),
-    "phone" CHAR(16) UNIQUE CHECK ("phone" ~* '^\+\d{1,15}$'),
+    "phone" VARCHAR(16) UNIQUE CHECK ("phone" ~* '^\+\d{1,15}$'),
     "salt" CHAR(22) NOT NULL,
     "passwd_hash" TEXT NOT NULL,
 
