bca6b56dfcc408240ba1501f696b9abd84eec557
sylvan <sylvanlyon@gmail.com>
2026-01-13 14:34:33 +0800

    feat(auth): distinguish expiration and others

--- backend/src/http/middleware/auth.rs
+++ backend/src/http/middleware/auth.rs
@@ -12,8 +12,12 @@ use axum::{
     response::{IntoResponse, Response},
 };
 use crab_vault_auth::{HttpMethod, Jwt, JwtDecoder, error::AuthError};
+use http::StatusCode;
 use serde::Deserialize;
+use serde_json::json;
 use tower::{Layer, Service};
+
+type PinBox<T> = Pin<Box<T>>;
 
 #[derive(Clone)]
 pub struct Auth<Inner, T, F>
@@ -26,10 +30,10 @@ where
             HttpMethod,
             &str,
             Jwt<T>,
-        ) -> Pin<Box<dyn Future<Output = Result<T, Response>> + Send>>,
+        ) -> PinBox<dyn Future<Output = Result<T, Response>> + Send>,
     T: 'static + Clone + Sync + Send + for<'de> Deserialize<'de>,
 {
-    inner: Inner,
+    inner_service: Inner,
     decoder: Arc<JwtDecoder>,
     validator: F,
     _p: PhantomData<T>,
@@ -46,71 +50,12 @@ where
             HttpMethod,
             &str,
             Jwt<T>,
-        ) -> Pin<Box<dyn Future<Output = Result<T, Response>> + Send>>,
+        ) -> PinBox<dyn Future<Output = Result<T, Response>> + Send>,
     T: 'static + Clone + Sync + Send + for<'de> Deserialize<'de>,
 {
     decoder: JwtDecoder,
     validator: F,
     _p: PhantomData<T>,
-}
-
-// 在 Inner 是一个 Service 的情况下，可以为 Auth<Inner> 实现 Service
-// 这个 Auth 和 Inner 使用同样的请求参数，axum::http::Request<ReqBody>
-impl<Inner, T, F> Service<Request> for Auth<Inner, T, F>
-where
-    Inner: Service<Request> + Send + Clone + 'static,
-    Inner::Error: std::error::Error,
-    Inner::Response: IntoResponse,
-    Inner::Future: 'static + Send,
-    F: 'static
-        + Clone
-        + Send
-        + Fn(
-            &HeaderMap,
-            HttpMethod,
-            &str,
-            Jwt<T>,
-        ) -> Pin<Box<dyn Future<Output = Result<T, Response>> + Send>>,
-    T: 'static + Clone + Sync + Send + for<'de> Deserialize<'de>,
-{
-    type Response = Response;
-    type Error = Infallible;
-    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
-
-    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        self.inner.poll_ready(cx).map_err(|_| unreachable!())
-    }
-
-    fn call(&mut self, mut req: Request) -> Self::Future {
-        let cloned = self.inner.clone();
-        let validator = self.validator.clone();
-        let decoder = self.decoder.clone();
-        let mut inner = std::mem::replace(&mut self.inner, cloned);
-
-        Box::pin(async move {
-            let call_inner_with_req = |req| async move {
-                match inner.call(req).await {
-                    Ok(val) => Ok(val.into_response()),
-                    Err(_) => unreachable!(),
-                }
-            };
-
-            match extract_token::<T>(req.headers(), decoder.as_ref()).await {
-                Ok(token) => {
-                    let (headers, method, path) =
-                        (req.headers(), req.method().into(), req.uri().path());
-                    match validator(headers, method, path, token).await {
-                        Ok(val) => {
-                            req.extensions_mut().insert(val);
-                        }
-                        Err(e) => return Ok(e),
-                    }
-                    call_inner_with_req(req).await
-                }
-                Err(e) => Ok(e),
-            }
-        })
-    }
 }
 
 impl<T, F> AuthLayer<T, F>
@@ -123,12 +68,12 @@ where
             HttpMethod,
             &str,
             Jwt<T>,
-        ) -> Pin<Box<dyn Future<Output = Result<T, Response>> + Send>>,
+        ) -> PinBox<dyn Future<Output = Result<T, Response>> + Send>,
     T: 'static + Clone + Sync + Send + for<'de> Deserialize<'de>,
 {
-    /// ## 此函数将在堆上创建一个 [`JwtConfig`] 结构作为这个中间件的配置
+    /// # 此函数将在堆上创建一个 [`JwtConfig`] 结构作为这个中间件的配置
     ///
-    /// ### 参数说明
+    /// ## 参数说明
     ///
     /// - `decoder`：解码 Jwt 的结构
     /// - `validator`：验证 token 上下文的
@@ -148,6 +93,68 @@ where
     }
 }
 
+// 在 Inner 是一个 Service 的情况下，可以为 Auth<Inner> 实现 Service
+// 这个 Auth 和 Inner 使用同样的请求参数，axum::http::Request<ReqBody>
+impl<Inner, Token, Valid> Service<Request> for Auth<Inner, Token, Valid>
+where
+    Inner: Service<Request> + Send + Clone + 'static,
+    Inner::Error: std::error::Error,
+    Inner::Response: IntoResponse,
+    Inner::Future: 'static + Send,
+    Valid: 'static
+        + Clone
+        + Send
+        + Fn(
+            &HeaderMap,
+            HttpMethod,
+            &str,
+            Jwt<Token>,
+        ) -> PinBox<dyn Future<Output = Result<Token, Response>> + Send>,
+    Token: 'static + Clone + Sync + Send + for<'de> Deserialize<'de>,
+{
+    type Response = Response;
+    type Error = Infallible;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
+
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.inner_service.poll_ready(cx).map_err(|_| unreachable!())
+    }
+
+    fn call(&mut self, mut req: Request) -> Self::Future {
+        let cloned = self.inner_service.clone();
+        let validate_token = self.validator.clone();
+        let decoder = self.decoder.clone();
+        let mut inner_service = std::mem::replace(&mut self.inner_service, cloned);
+
+        Box::pin(async move {
+            let (headers, method, path) = (req.headers(), req.method().into(), req.uri().path());
+
+            match extract_token::<Token>(headers, &decoder).await {
+                Ok(token) => match validate_token(headers, method, path, token).await {
+                    Ok(ext) => {
+                        req.extensions_mut().insert(ext);
+                        match inner_service.call(req).await {
+                            Ok(response) => Ok(response.into_response()),
+                            Err(_) => unreachable!(),
+                        }
+                    }
+                    Err(e) => Ok(e),
+                },
+                Err(e) => match e {
+                    AuthError::TokenExpired => Ok((
+                        StatusCode::UNAUTHORIZED,
+                        axum::Json(json!({
+                            "reason": "tokenExpired"
+                        })),
+                    )
+                        .into_response()),
+                    _ => Ok(e.into_response()),
+                },
+            }
+        })
+    }
+}
+
 impl<Inner, T, F> Layer<Inner> for AuthLayer<T, F>
 where
     F: 'static
@@ -158,7 +165,7 @@ where
             HttpMethod,
             &str,
             Jwt<T>,
-        ) -> Pin<Box<dyn Future<Output = Result<T, Response>> + Send>>,
+        ) -> PinBox<dyn Future<Output = Result<T, Response>> + Send>,
     T: 'static + Clone + Sync + Send + for<'de> Deserialize<'de>,
 {
     type Service = Auth<Inner, T, F>;
@@ -171,7 +178,7 @@ where
         } = self.clone();
 
         Auth {
-            inner,
+            inner_service: inner,
             validator: validator.clone(),
             decoder: Arc::new(decoder),
             _p: PhantomData,
@@ -180,7 +187,7 @@ where
 }
 
 /// 提取并验证JWT令牌
-async fn extract_token<T>(headers: &HeaderMap, decoder: &JwtDecoder) -> Result<Jwt<T>, Response>
+async fn extract_token<T>(headers: &HeaderMap, decoder: &JwtDecoder) -> Result<Jwt<T>, AuthError>
 where
     T: 'static + Clone + Sync + Send + for<'de> Deserialize<'de>,
 {
@@ -196,5 +203,5 @@ where
         .strip_prefix("Bearer ")
         .ok_or(AuthError::InvalidAuthFormat)?;
 
-    Ok(decoder.decode(token)?)
+    decoder.decode(token)
 }
